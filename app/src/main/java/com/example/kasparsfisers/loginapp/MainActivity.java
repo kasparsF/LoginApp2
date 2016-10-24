567d01ab29b47c5104a08f12670b43ca4ddc4f27
kaspars.fisers <kasparsfisers@inbox.lv>
2016-10-24 14:50:39 +0300

    location detector

--- app/src/main/java/com/example/kasparsfisers/loginapp/MainActivity.java
+++ app/src/main/java/com/example/kasparsfisers/loginapp/MainActivity.java
@@ -16,10 +16,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-EditText logUser, logPass;
-    Button login,register;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText logUser, logPass;
+    Button login, register, location;
     ImageView imgLoading;
+    int m = 0;
     FragmentManager fm = getSupportFragmentManager();
 
     ObjectAnimator a = new ObjectAnimator();
@@ -30,62 +31,79 @@ EditText logUser, logPass;
         setContentView(R.layout.activity_main);
         logUser = (EditText) findViewById(R.id.loginUser);
         logPass = (EditText) findViewById(R.id.loginPass);
-        login = (Button)findViewById(R.id.btnLogin);
-        register =(Button)findViewById(R.id.btnLoginReg);
+        login = (Button) findViewById(R.id.btnLogin);
+        register = (Button) findViewById(R.id.btnLoginReg);
+        location = (Button) findViewById(R.id.btnLocation);
         login.setOnClickListener(this);
         register.setOnClickListener(this);
-        imgLoading = (ImageView)findViewById(R.id.imageRotate);
+        location.setOnClickListener(this);
+        imgLoading = (ImageView) findViewById(R.id.imageRotate);
 
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.btnLogin){
+        if (v.getId() == R.id.btnLogin) {
 
-            Animation hyperspaceJump = AnimationUtils.loadAnimation(this, R.anim.rotate);
-            imgLoading.startAnimation(hyperspaceJump);
+            Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+            imgLoading.startAnimation(animation);
 
             login.setVisibility(View.GONE);
             register.setVisibility(View.GONE);
             logUser.setVisibility(View.GONE);
             logPass.setVisibility(View.GONE);
+            location.setVisibility(View.GONE);
             Handler handler = new Handler(getMainLooper());
-            handler.postDelayed(new Runnable(){
+            handler.postDelayed(new Runnable() {
                 public void run() {
 
-            String user = logUser.getText().toString();
-            String pass = logPass.getText().toString();
+                    String user = logUser.getText().toString();
+                    String pass = logPass.getText().toString();
 
-            SharedPreferences preferences = getSharedPreferences("MYPREFS", MODE_PRIVATE);
-            String userDetails = preferences.getString(user + pass + "info", "");
-            if (userDetails.equals("")){
-                Toast.makeText(MainActivity.this, "Wrong data provided", Toast.LENGTH_SHORT).show();
-                login.setVisibility(View.VISIBLE);
-                register.setVisibility(View.VISIBLE);
-                logUser.setVisibility(View.VISIBLE);
-                logPass.setVisibility(View.VISIBLE);
-            }else {
+                    SharedPreferences preferences = getSharedPreferences("MYPREFS", MODE_PRIVATE);
+                    String userDetails = preferences.getString(user + pass + "info", "");
+                    if (userDetails.equals("")) {
+                        Toast.makeText(MainActivity.this, "Wrong data provided", Toast.LENGTH_SHORT).show();
+                        login.setVisibility(View.VISIBLE);
+                        register.setVisibility(View.VISIBLE);
+                        logUser.setVisibility(View.VISIBLE);
+                        logPass.setVisibility(View.VISIBLE);
+                        location.setVisibility(View.VISIBLE);
+                    } else {
 
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("display", userDetails);
-                editor.commit();
-                Intent toLogin = new Intent(MainActivity.this,Display.class);
-                startActivity(toLogin);
-                login.setVisibility(View.VISIBLE);
-                register.setVisibility(View.VISIBLE);
-                logUser.setVisibility(View.VISIBLE);
-                logPass.setVisibility(View.VISIBLE);
-            }
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("display", userDetails);
+                        editor.commit();
+                        Intent toLogin = new Intent(MainActivity.this, Display.class);
+                        startActivity(toLogin);
+                        login.setVisibility(View.VISIBLE);
+                        register.setVisibility(View.VISIBLE);
+                        logUser.setVisibility(View.VISIBLE);
+                        logPass.setVisibility(View.VISIBLE);
+                        location.setVisibility(View.VISIBLE);
+                    }
 
                 }
             }, 3000);
 
         }
 
-        if(v.getId() == R.id.btnLoginReg){
+        if (v.getId() == R.id.btnLoginReg) {
             AlertDFragment alertdFragment = new AlertDFragment();
             // Show Alert DialogFragment
             alertdFragment.show(fm, "Alert Dialog Fragment");
+
+        }
+
+        if (v.getId() == R.id.btnLocation) {
+            if (m == 0) {
+                startService(new Intent(getBaseContext(), LocationService.class));
+                m = 1;
+            } else {
+                stopService(new Intent(getBaseContext(), LocationService.class));
+                m = 0;
+            }
+
 
         }
     }
