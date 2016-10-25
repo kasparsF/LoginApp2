5a7eacd4084fc40509c078f54a48f98e31cfb4d0
kaspars.fisers <kasparsfisers@inbox.lv>
2016-10-25 14:13:27 +0300

    finished app

--- app/src/main/java/com/example/kasparsfisers/loginapp/LocationService.java
+++ app/src/main/java/com/example/kasparsfisers/loginapp/LocationService.java
@@ -130,7 +130,7 @@ public class LocationService extends Service implements GoogleApiClient.Connecti
                 Double.toString(location.getAccuracy());
 
         insertCoordinates(location.getLatitude(),location.getLongitude(),location.getAccuracy());
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
 
     }
 
